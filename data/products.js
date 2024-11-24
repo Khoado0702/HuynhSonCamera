e69421dde847e75da2c4d119e464f6a4aec62c23
HuynhSonCamera <140762146+Khoado0702@users.noreply.github.com>
2024-11-24 19:24:25 +0700

    Add files via upload

---
+++ data/products.js
@@ -0,0 +1,300 @@
+var list_products = [ {
+    "name": "IP Wifi 2MP Dahua", //Ten Camera .Camera IP Wifi 2MP Dahua DH-IPC-HFW1230DT-STW
+    "company": "Dahua", // Hang Camera
+    "img": "img/products/Dahua-1.jpg", // Them anh
+    "price": "800.000", // Gia tien
+    "star": 4,  // So sao
+    "rateCount": 11, // Luot danh gia
+    "promo": {
+        "name": "giamgia", // Loai hang
+        "value": "200.000" // Gia giam
+    },
+    "detail": {
+        "screen": "chuẩn nén H265+, 25/30fps 1080px", // Do phan giai va fps
+        "os": "Camera hỗ trợ Mic và loa, đàm thoại 2 chiều - Chế độ ngày đêm (ICR), chống ngược sáng DWDR", // Tien ich
+        "camara": "", // Kich thuoc
+        "camaraFront": "Hỗ trợ WiFi 2.4GHz", // Bang tan wifi
+        "cpu": "Phần mềm Dahua", // Phan mem
+        "ram": "30m", // Tam nhin xa hong ngoai
+        "rom": "Hỗ trợ SD lên đến 256Gb - DC 12V", // Luu tru va nguon dien
+        "microUSB": "1/3 inch CMOS", // Cam bien hinh anh
+        "battery": "IP67" // Chuan chong nuoc
+    },
+    "masp": "Opp1"
+},{
+    "name": "O Cung", //Ten Camera
+    "company": "O_Cung", // Hang Camera
+    "img": "img/products/Hik-1.jpg", // Them anh
+    "price": "2.490.000", // Gia tien
+    "star": 4, // So sao
+    "rateCount": 80, // Luot danh gia
+    "promo": {
+        "name": "O_Cung", // Loai hang
+        "value": "" // Gia giam
+    },
+    "detail": {
+        "screen": "sdfsd", // Do phan giai va fps
+        "os": "", // Tien ich
+        "camara": "", // Kich thuoc
+        "camaraFront": "", // Bang tan wifi
+        "cpu": "", // Phan mem
+        "ram": "", // Tam nhin xa hong ngoai
+        "rom": "", // Luu tru va nguon dien
+        "microUSB": "", // Cam bien hinh anh
+        "battery": "" // Chuan chong nuoc
+    },
+    "masp": "Mob2"
+},{
+    "name": "Ezviz H8c Pro 2K (3.0MP)", //Ten Camera
+    "company": "Ezviz", // Hang Camera
+    "img": "img/products/Ezviz-2.jpg", // Them anh
+    "price": "950.000", // Gia tien
+    "star": 4, // So sao
+    "rateCount": 43, // Luot danh gia
+    "promo": {
+        "name": "giamgia", // Loai hang
+        "value": "300.000" // Gia giam
+    },
+    "detail": {
+        "screen": "Độ phân giải 2304 × 1296 @ 30fps", // Do phan giai va fps
+        "os": "Theo dõi chuyển động thông minh", // Tien ich
+        "camara": "Ống kính 4mm@ F1.6, 104°(Chéo), 89° (Ngang), 49° (Dọc)", // Kich thuoc
+        "camaraFront": "Hỗ trợ  WiFi , IEEE802.11b, 802.11g, 802.11n , tần số 2.4GHz", // Bang tan wifi
+        "cpu": "Phần mềm Ezviz", // Phan mem
+        "ram": "Quay ngang 350°, dọc 80° - Hồng ngoại 30m, đèn trợ sáng 20m", // Tam nhin xa hong ngoai
+        "rom": "Hỗ trợ khe cắm thẻ nhớ đến 512GB", // Luu tru va nguon dien
+        "microUSB": "Chuấn nén H.265/H.264", // Cam bien hinh anh
+        "battery": "Tiêu chuẩn IP65" // Chuan chong nuoc
+    },
+    "masp": "Sam3"
+}, {
+    "name": " Hikvision IP 2MP H265/H265+ WIFI", //Ten Camera
+    "company": "Hikvision", // Hang Camera
+    "img": "img/products/Hik-1.jpg", // Them anh
+    "price": "3.000.000", // Gia tien
+    "star": 0, // So sao
+    "rateCount": 0, // Luot danh gia
+    "promo": {
+        "name": "giamgia", // Loai hang
+        "value": "300.000" // Gia giam
+    },
+    "detail": {
+        "screen": "(1920x1080) - 25fps/30fps", // Do phan giai va fps
+        "os": "WiFi tối đa 50m - Tích hợp Giao tiếp micro hai chiều", // Tien ich
+        "camara": "99x6866mm - 272g - Ống kính 2.8/4mm", // Kich thuoc
+        "camaraFront": "2.4GHz", // Bang tan wifi
+        "cpu": "Hik-connect", // Phan mem
+        "ram": "30m", // Tam nhin xa hong ngoai
+        "rom": "Tối đa 128Gb - 12V/DC", // Luu tru va nguon dien
+        "microUSB": "1/2.8 inch CMOS", // Cam bien hinh anh
+        "battery": "IP66" // Chuan chong nuoc
+    },
+    "masp": "App3"
+}, {
+    "name": "Ezviz H3C 2K (3MP)", //Ten Camera
+    "company": "Ezviz", // Hang Camera
+    "img": "img/products/Ezviz-1.jpg", // Them anh
+    "price": "830.000", // Gia tien
+    "star": 4, // So sao
+    "rateCount": 804, // Luot danh gia
+    "promo": {
+        "name": "giamgia", // Loai hang
+        "value": "200.000" // Gia giam
+    },
+    "detail": {
+        "screen": " 2304 × 1296 @ 30fps", // Do phan giai va fps
+        "os": "Tích hợp Micro và loa, hỗ trợ đàm thoại 2 chiều - Hỗ trợ phát hiện hình dạng người, phát hiện chuyển động", // Tien ich
+        "camara": "Ống kính 4mm@ F2.0, 98°(Chéo), 82° (Ngang)", // Kich thuoc
+        "camaraFront": "Hỗ trợ  WiFi , IEEE802.11b, 802.11g, 802.11n , tần số 2.4GHz", // Bang tan wifi
+        "cpu": "Phần mềm EZVIZ", // Phan mem
+        "ram": "30m", // Tam nhin xa hong ngoai
+        "rom": "Hỗ trợ khe cắm thẻ nhớ đến 512GB", // Luu tru va nguon dien
+        "microUSB": "Chuấn nén H.265/H.264", // Cam bien hinh anh
+        "battery": "Tiêu chuẩn IP67" // Chuan chong nuoc
+    },
+    "masp": "Hua3"
+},{
+    "name": "Imou IPC S2XP 10M0WED", //Ten Camera
+    "company": "Imou", // Hang Camera
+    "img": "img/products/Imou-1.jpg", // Them anh
+    "price": "1.200.000", // Gia tien
+    "star": 4, // So sao
+    "rateCount": 13, // Luot danh gia
+    "promo": {
+        "name": "tragop", // Loai hang
+        "value": "" // Gia giam
+    },
+    "detail": {
+        "screen": "10MP - Tốc độ khung hình 15fps.", // Do phan giai va fps
+        "os": "Ống kính quay quét có thể điều khiển từ xa, góc quay ngang 0~355°, góc quay dọc 0~90°. Trang bị ống kính cố định 3.6mm (góc nhìn 84°)", // Tien ich
+        "camara": "2 Ống kính (5.0 MP ống kính cố định; 5.0 MP ống kính quay quét).", // Kich thuoc
+        "camaraFront": "Tích hợp Wi-Fi 6 2.4GHz, Cổng LAN", // Bang tan wifi
+        "cpu": "Phần mềm Imou", // Phan mem
+        "ram": "Hồng ngoại kèm LED tầm xa 15m", // Tam nhin xa hong ngoai
+        "rom": "Khe cắm thẻ nhớ Micro SD max 256GB - DC 5V1.5A", // Luu tru va nguon dien
+        "microUSB": "Chuẩn nén H.265", // Cam bien hinh anh
+        "battery": "Tiêu chuẩn trong nhà" // Chuan chong nuoc
+    },
+    "masp": "e2"
+},{
+    "name": "IMOU IPC S7DP 5M0WEZ 5MP(Cruiser Z 3K)", //Ten Camera
+    "company": "Imou", // Hang Camera
+    "img": "img/products/Imou-2.jpg", // Them anh
+    "price": "1.600.000", // Gia tien
+    "star": 4, // So sao
+    "rateCount": 13, // Luot danh gia
+    "promo": {
+        "name": "moiramat", // Loai hang
+        "value": "" // Gia giam
+    },
+    "detail": {
+        "screen": "Độ phân giải 5MP - 15fps@5.0M", // Do phan giai va fps
+        "os": "Chống ngược sáng HDR - 4 chế độ sáng ban đêm: Tự động, Full Color, Hồng ngoại và tắt - Đèn và hú còi (110dB) khi phát hiện có đối tượng xâm nhập - Tích hợp mic và loa, hỗ trợ đàm thoại 2 chiều to rõ", // Tien ich
+        "camara": "2 Ống kính (5.0 MP ống kính cố định; 5.0 MP ống kính quay quét).", // Kich thuoc
+        "camaraFront": "Hỗ trợ chuẩn ONVIF, Cloud, LAN, Kết nối Wi-Fi 6 với 2 ăng-ten", // Bang tan wifi
+        "cpu": "Phần mềm Imou", // Phan mem
+        "ram": "Tầm xa chiếu sáng ban đêm lên đến 56m", // Tam nhin xa hong ngoai
+        "rom": "Khe cắm thẻ nhớ Micro SD lên đến 512GB - DC12V1A, Công suất <12 W", // Luu tru va nguon dien
+        "microUSB": "Chuẩn nén H.265 - 1/3” CMOS - Hỗ trợ quay quét: ngang 0-355°, dọc 0-90°", // Cam bien hinh anh
+        "battery": "Chống bụi nước IP66" // Chuan chong nuoc
+    },
+    "masp": "53"
+},{
+    "name": "Kbvision KXC2121S5 2MP", //Ten Camera
+    "company": "Kbvision", // Hang Camera
+    "img": "img/products/Kbvision-1.jpg", // Them anh
+    "price": "1.200.000", // Gia tien
+    "star": 4, // So sao
+    "rateCount": 4, // Luot danh gia
+    "promo": {
+        "name": "moiramat", // Loai hang
+        "value": "" // Gia giam
+    },
+    "detail": {
+        "screen": "1920x1080 - 1080P@25/30fps; 720P@25/30fps; 720P@50/60fps.", // Do phan giai va fps
+        "os": "Hỗ trợ chức năng cảm biến ngày đêm, tự động cân bằng, chống ngược sáng, giảm nhiễu 2DNR.", // Tien ich
+        "camara": "Ống kính cố định: 3.6mm (góc nhìn 84°).", // Kich thuoc
+        "camaraFront": "Hỗ trợ chuẩn ONVIF, Cloud, LAN, Kết nối Wi-Fi 6 với 2 ăng-ten", // Bang tan wifi
+        "cpu": "Phần mềm Kbvision", // Phan mem
+        "ram": "30 mét", // Tam nhin xa hong ngoai
+        "rom": "Đầu ghi - 12VDC ±30%", // Luu tru va nguon dien
+        "microUSB": "1/2.7 inch Sony", // Cam bien hinh anh
+        "battery": "Chống bụi nước IP67" // Chuan chong nuoc
+    },
+    "masp": "533"
+},{
+    "name": "Ezviz 2MP Quay Quet 360", //Ten Camera
+    "company": "Ezviz", // Hang Camera
+    "img": "img/products/Ezviz-3.jpg", // Them anh
+    "price": "1.100.000", // Gia tien
+    "star": 4, // So sao
+    "rateCount": 4, // Luot danh gia
+    "promo": {
+        "name": "tragop", // Loai hang
+        "value": "" // Gia giam
+    },
+    "detail": {
+        "screen": "2MP - 1920×1080 @ 15fps", // Do phan giai va fps
+        "os": "Hỗ trợ tính năng theo dõi thông minh - Hỗ trợ DWDR, 3D DNR , BLC , ICR - Phát hiện chuyển động thông minh - Tích hợp Micro và Loa – Hỗ trợ đàm thoại 2 chiều với chất lượng âm thanh trung thực", // Tien ich
+        "camara": "Góc nhìn ngang 4mm@ F2.4, 85°(Chéo), 75° (Ngang),45°(Dọc) - Góc quay ngang 340 độ , góc xoay dọc 55 độ", // Kich thuoc
+        "camaraFront": "Hỗ trợ wifi , IEEE802.11b, 802.11g, 802.11n , tần số 2.4GHz", // Bang tan wifi
+        "cpu": "Phần mềm Ezviz", // Phan mem
+        "ram": "30 mét", // Tam nhin xa hong ngoai
+        "rom": "Hỗ trợ khe cắm thẻ nhớ đến 256GB", // Luu tru va nguon dien
+        "microUSB": "Tiêu chuẩn Ezviz", // Cam bien hinh anh
+        "battery": "Tiêu chuẩn trong nhà" // Chuan chong nuoc
+    },
+    "masp": "424"
+},{
+    "name": "IP Dahua DH_IPC_HDW1431T1_A", //Ten Camera
+    "company": "Dahua", // Hang Camera
+    "img": "img/products/Dahua-2.jpg", // Them anh
+    "price": "1.400.000", // Gia tien
+    "star": 4, // So sao
+    "rateCount": 10, // Luot danh gia
+    "promo": {
+        "name": "tragop", // Loai hang
+        "value": "" // Gia giam
+    },
+    "detail": {
+        "screen": "2560 × 1440 @ 25/30fps - 4.0MP", // Do phan giai va fps
+        "os": "Phát hiện con người - Microphone - Chống ngược sáng: WDR thực 120dB", // Tien ich
+        "camara": " Cố định 2.8mm (góc nhìn 92°)", // Kich thuoc
+        "camaraFront": "tần số 2.4GHz", // Bang tan wifi
+        "cpu": "Phần mềm Ezviz", // Phan mem
+        "ram": "30m, hỗ trợ hồng ngoại thông minh", // Tam nhin xa hong ngoai
+        "rom": "12VDC, hỗ trợ PoE", // Luu tru va nguon dien
+        "microUSB": "1/2.9″ CMOS - H.265+", // Cam bien hinh anh
+        "battery": "Vỏ kim loại và nhựa, chuẩn chống nước và bụi IP67" // Chuan chong nuoc
+    },
+    "masp": "494"
+},{
+    "name": "IP Dahua DH_IPC_HFW1230SP_S5_VN", //Ten Camera
+    "company": "Dahua", // Hang Camera
+    "img": "img/products/Dahua-3.jpg", // Them anh
+    "price": "800.000", // Gia tien
+    "star": 4, // So sao
+    "rateCount": 2, // Luot danh gia
+    "promo": {
+        "name": "tragop", // Loai hang
+        "value": "100.000" // Gia giam
+    },
+    "detail": {
+        "screen": "2MP", // Do phan giai va fps
+        "os": "Phát hiện con người - Microphone - Chống ngược sáng: WDR thực 120dB", // Tien ich
+        "camara": "Ống kính 3,6mm - ", // Kich thuoc
+        "camaraFront": "tần số 2.4GHz", // Bang tan wifi
+        "cpu": "Hỗ trợ xem qua giao diện Web mà không cần plug-in (Chrome, Firefox,...)", // Phan mem
+        "ram": "30m, hỗ trợ hồng ngoại thông minh", // Tam nhin xa hong ngoai
+        "rom": "Điện áp DC12V hoặc PoE (802.3af)E", // Luu tru va nguon dien
+        "microUSB": "CMOS kích thước 1/2.8 inch - H.265 và H.264", // Cam bien hinh anh
+        "battery": "Vỏ kim loại và nhựa, chuẩn chống nước và bụi IP67" // Chuan chong nuoc
+    },
+    "masp": "124"
+},{
+    "name": "Speed IP Mini Hikvision DS_2CD1P23G2_IUF", //Ten Camera
+    "company": "Hikvision", // Hang Camera
+    "img": "img/products/Hikvision-2.jpg", // Them anh
+    "price": "1.300.000", // Gia tien
+    "star": 4, // So sao
+    "rateCount": 5, // Luot danh gia   
+    "promo": {
+        "name": "moiramat", // Loai hang
+        "value": "100.000" // Gia giam
+    },
+    "detail": {
+        "screen": "1920 × 1080@25fps - 2MP - Xoay: -100° đến 100°; Lên/xuống: -20° đến 45°", // Do phan giai va fps
+        "os": "Hỗ trợ nút reset cứng - Có tích hợp micro thu âm - 120dB WDR, AGC, BLC, 3D DNR, HLC", // Tien ich
+        "camara": "Cố định 2.8mm / 4mm", // Kich thuoc
+        "camaraFront": "tần số 2.4GHz", // Bang tan wifi
+        "cpu": "Hỗ trợ xem qua giao diện Web mà không cần plug-in (Chrome, Firefox,...)", // Phan mem
+        "ram": "EXIR 2.0, 30m", // Tam nhin xa hong ngoai
+        "rom": "microSD, tối đa 256GB - 12VDC ±25%, PoE", // Luu tru va nguon dien
+        "microUSB": "1/2.8″ Progressive Scan CMOS - H.265+", // Cam bien hinh anh
+        "battery": "IP66 (phù hợp cho sử dụng ngoài trời)" // Chuan chong nuoc
+    },
+    "masp": "224"
+},{
+    "name": "IP Hong Ngoai 4MP HIKVISION DS_2CD2345G0P_I", //Ten Camera
+    "company": "Hikvision", // Hang Camera
+    "img": "img/products/Hikvision-3.jpg", // Them anh
+    "price": "3.200.000", // Gia tien
+    "star": 4, // So sao
+    "rateCount": 8, // Luot danh gia   
+    "promo": {
+        "name": "moiramat", // Loai hang
+        "value": "100.000" // Gia giam
+    },
+    "detail": {
+        "screen": "4MP (2688 x 1520 25fps)", // Do phan giai va fps
+        "os": "Chống nhiễu kỹ thật số 3D DNR, chống ngược sáng thực 120dB WDR, tính năng ICR, Chức năng bù ngược sáng BLC. Tự chuyển chế độ ngày/đêm, Phát hiện xâm nhập khu vực cấm, vượt hàng rào ảo, phát hiện hành lý vô chủ, phát hiện di dời vật thể, phát hiện khuôn mặt", // Tien ich
+        "camara": "Φ 127.3 x 104.1 mm", // Kich thuoc
+        "camaraFront": "tần số 2.4GHz", // Bang tan wifi
+        "cpu": "Phần mềm Hikvision", // Phan mem
+        "ram": "10m", // Tam nhin xa hong ngoai
+        "rom": "Hỗ trợ thẻ nhớ lên đến 256G - 12 VDC ± 25%, PoE (802.3af, type 1, class 3)", // Luu tru va nguon dien
+        "microUSB": "H.265+/H.264+/H.265/H.264", // Cam bien hinh anh
+        "battery": "Tiêu chuẩn trong nhà" // Chuan chong nuoc
+    },
+    "masp": "274"
+}]
